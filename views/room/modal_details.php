132a7013ad0bfda0529fadda833be6d915b3bedd
ettienne <12529192+jhweb@users.noreply.github.com>
2025-12-28 15:11:38 +0200

    fix: use ModalButton::cancel and data-modal-close for correct modal closing

--- views/room/modal_details.php
+++ views/room/modal_details.php
@@ -15,7 +15,7 @@ if (!empty($stream->end_time)) {
 <div class="modal-dialog modal-dialog-medium animated fadeIn">
     <div class="modal-content">
         <div class="modal-header">
-            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+            <button type="button" class="close" data-dismiss="modal" data-modal-close aria-hidden="true">&times;</button>
             <h4 class="modal-title" id="myModalLabel">
                 <?= Yii::t('JitsiMeetCloud8x8Module.base', 'Stream Recording & Downloads'); ?>
             </h4>
@@ -102,7 +102,7 @@ if (!empty($stream->end_time)) {
 
         </div>
         <div class="modal-footer">
-            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            <?= ModalButton::cancel('Close') ?>
         </div>
     </div>
 </div>
